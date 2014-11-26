28c91375b28fdf8c0b5a798d97bb458ef0882653
Wendy Nichols <wendlelyn@gmail.com>
2014-11-26 11:19:34 -0500

    added in parse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('The Iron Yard Rocks');
+(function () {
+
+  angular.module('', ['ngRoute']).constant('PARSE_HEADERS', {
+    headers: {
+    "X-Parse-Application-Id: itgnj31wh8MQhIxiy6OQwFWOlzeVWZt56SipOGe8",
+    "X-Parse-REST-API-Key: SGRofGlAL5zgOStCjRsdhrVbQKetIzmjIFqz1RaM",
+    "Content-Type: application/json"
+  }
+  })
+
+  .config( function ($routeProvider) {
+
+  });
+
+}());
